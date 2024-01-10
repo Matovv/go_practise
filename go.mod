10409248ecfd9e32d0b6d5b827523ef87af38dcc
Elnur Shamiyev <elnur@Macbooks-MacBook-Air.local>
2024-01-11 02:34:26 +0400

    new functionality, added dependencies

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Matovv/go_practise
+
+go 1.21.5
+
+require github.com/Matovv/helper-repo-1 v0.0.0-20240110222235-bb180dee29ca
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Matovv/helper-repo-1"
+)
+
+func main() {
+	fmt.Println("Hello People 😂")
+	x := helperRepo1.Multiply(3,10)
+	fmt.Println(x)
+}
