5801ef89920c801ca2341ddd5ff2be808214b41f
Elnur Shamiyev <elnur@Macbooks-MacBook-Air.local>
2024-01-11 16:20:37 +0400

    updated main function

--- main.go
+++ main.go
@@ -9,4 +9,18 @@ func main() {
 	fmt.Println("Hello People 😂")
 	x := helperRepo1.Multiply(3,10)
 	fmt.Println(x,helperRepo1.Version15())
+
+	xi := []string{"lol", "back", "assman"}
+	m := map[string]int{
+		"civi": 232,
+		"cumshet": 999,
+	}
+
+	for i,v := range xi {
+		fmt.Println("index is - ", i, " and value is - ", v)
+	}
+	for k,v := range m {
+		fmt.Println("key is - ", k, " and value is - ", v)
+	}
+	
 }
