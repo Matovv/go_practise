b2491150cea0be05bfbf2528435e0e526b359874
Elnur Shamiyev <elnur.shamiyev@gmail.com>
2024-01-11 22:29:45 +0400

    updated functionality

--- main.go
+++ main.go
@@ -7,6 +7,15 @@ import (
 	"sort"
 )
 
+type Engine struct {
+	model string
+	power float32
+}
+
+func (e *Engine) Start() {
+	fmt.Println("Engine",e.model, "started")
+} 
+
 func main() {
 	fmt.Println("Hello People 😂")
 	x := helperRepo1.Multiply(3,10)
@@ -29,6 +38,14 @@ func main() {
 
 	xiping := []int{5,9,2,12}
 	fmt.Println(SortAndReturn(xiping))
+
+	newEngine := Engine{
+		model: "Schwarz3",
+		power: 75,
+	}
+
+	fmt.Println("Engine model -",newEngine.model, "and power -",newEngine.power)
+	newEngine.Start();
 	
 }
 
