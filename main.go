0e4cb5b66f3daf7325bb3d1ea5a12920c9b718b2
Elnur Shamiyev <elnur.shamiyev@gmail.com>
2024-01-13 00:58:21 +0400

    fixed pointer functions

--- main.go
+++ main.go
@@ -55,9 +55,10 @@ func main() {
 	fmt.Println("Engine model -",newEngine.model, "and power -",newEngine.power)
 	newEngine.Start();
 
-	someVar := 5.0;
-    fmt.Println("Modified -",Multiply(someVar))
-    fmt.Println("Original -",someVar)
+	someVar := 10.0;
+    fmt.Println("Original before -",someVar)
+    fmt.Println("Modified -",Multiply(&someVar))
+    fmt.Println("Original after -",someVar)
 
 	var mt MyType = 5;
 	log.Println(mt)
@@ -65,13 +66,12 @@ func main() {
 	log.Println(mt)
 	mt.Multi()
 	log.Println(mt)
-
 	
 }
 
-func Multiply(x float64) float64 {
-	x *= 5
-	return x
+func Multiply(x *float64) float64 {
+	*x *= 5
+	return *x
 }
 
 func SortAndReturn(x []int) []int {
