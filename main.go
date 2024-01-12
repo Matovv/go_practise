a6c6c533da62c69916efe6ca88220b670637e70a
Elnur Shamiyev <elnur.shamiyev@gmail.com>
2024-01-12 19:30:36 +0400

    new functionality

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/Matovv/helper-repo-1"
+	"log"
 	"math/rand"
 	"sort"
+
+	"github.com/Matovv/helper-repo-1"
 )
 
 type Engine struct {
@@ -15,6 +17,12 @@ type Engine struct {
 func (e *Engine) Start() {
 	fmt.Println("Engine",e.model, "started")
 } 
+
+type MyType int
+
+func (mt MyType) Multi() MyType{
+	return mt*2
+}
 
 func main() {
 	fmt.Println("Hello People 😂")
@@ -46,7 +54,21 @@ func main() {
 
 	fmt.Println("Engine model -",newEngine.model, "and power -",newEngine.power)
 	newEngine.Start();
+
+	someVar := 5.0;
+    fmt.Println("Modified -",Multiply(someVar))
+    fmt.Println("Original -",someVar)
+
+	var mt MyType = 5;
+
+	log.Println(mt.Multi())
+
 	
+}
+
+func Multiply(x float64) float64 {
+	x *= 5
+	return x
 }
 
 func SortAndReturn(x []int) []int {
